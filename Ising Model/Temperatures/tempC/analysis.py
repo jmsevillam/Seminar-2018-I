b78621273a3603e35082b66f423d0265cac08a86
jmsevillam <jmsevillam@unal.edu.co>
2018-06-30 09:31:51 -0500

    Field zero program

--- Ising Model/Temperatures/tempC/analysis.py
+++ Ising Model/Temperatures/tempC/analysis.py
@@ -2,18 +2,18 @@ from os import walk
 import numpy as np
 def hasNumbers(inputString):
     return any(char.isdigit() for char in inputString)
-for (dirpath,dirnames,filenames) in walk('.'):
+folder='mag/'
+for (dirpath,dirnames,filenames) in walk(folder+'.'):
     files=filenames
     break
 print files
 M=[]
 T=[]
 files.sort()
-num=100
+num=300
 for i in files:
-    if(hasNumbers(i)):
         print(i)
-        data=np.genfromtxt(i)
+        data=np.genfromtxt(folder+i)
         dataMag=data[:]
         T.append(float(i))
         Maverage=sum(dataMag[-int(num):])/num
@@ -23,4 +23,26 @@ import matplotlib.pylab as plt
 plt.plot(T,M,'.-')
 plt.savefig('plot1.pdf')
 plt.savefig('plot1.png')
+plt.show()
+folder='sus/'
+for (dirpath,dirnames,filenames) in walk(folder+'.'):
+    files=filenames
+    break
+print files
+M=[]
+T=[]
+files.sort()
+num=100
+for i in files:
+        print(i)
+        data=np.genfromtxt(folder+i)
+        dataMag=data[:]
+        T.append(float(i))
+        Maverage=sum(dataMag[-int(num):])/num
+        M.append(Maverage)
+import matplotlib.pylab as plt
+
+plt.plot(T,M,'.-')
+plt.savefig('plot2.pdf')
+plt.savefig('plot2.png')
 plt.show()
--- Ising Model/Temperatures/tempC/scripta.sh
+++ Ising Model/Temperatures/tempC/scripta.sh
@@ -1,6 +1,12 @@
 g++ ising.cpp
-for i in `LANG=en_US seq 0.05 0.01 4.5`
+rm sus/*
+rm mag/*
+for i in `LANG=en_US seq 0.05 0.05 4.5`
 do
 echo $i
-time  ./a.out $i 
+time  ./a.out $i sus$i
+mv sus$i sus/$i
+mv $i mag/$i
 done
+rm sus/*,*
+rm mag/*,*
