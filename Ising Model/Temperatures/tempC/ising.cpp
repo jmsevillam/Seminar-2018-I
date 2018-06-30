32b9876a37011fa2ad3fabefbb9395646b0664ea
jmsevillam <jmsevillam@unal.edu.co>
2018-06-30 09:34:49 -0500

    Field zero code

--- Ising Model/Temperatures/tempC/ising.cpp
+++ Ising Model/Temperatures/tempC/ising.cpp
@@ -3,34 +3,40 @@
 #include<cmath>
 #include<fstream>
 #include<cstdlib>
-const int Nx=100;
-const int Ny=100;
+const int Nx=25;
+const int Ny=25;
 const int N=1000;
 const double J=1.;
-const double h=0.1;
+const double h=.1;
 typedef std::vector<std::vector<double> > matrix;
 void get_mem(matrix & m);
 void print(matrix & m);
-void Time_Step(matrix & m,const double kbT);
+void Time_Step(matrix & m,const double kbT,const double H);
 void print_file(const char * NameArch, matrix & M);
 double calculate_Mag(const matrix & m);
+double calculate_sus(const matrix & m);
 int main(int argc, char * argv[]){
   const double kbT=atof(argv[1]);
   srand(1);
   matrix ising;
   get_mem(ising);
   int Number=Nx*Ny;
-  
+  double mag=0;
+  double H=h;
   std::ofstream File(argv[1]);
-  File.precision(16);
-  
+  std::ofstream File2(argv[2]);
+  File.precision(16); File2.precision(16);  
   for (int t=0;t<N*Number;t++){
-    Time_Step(ising,kbT);
+    if(t==(int) N*Number/2.){H=0.;}
+    Time_Step(ising,kbT,H);
     if(t%Number==0){
-      File<<calculate_Mag(ising)<<std::endl;
+      mag=calculate_Mag(ising);
+      File<<mag<<std::endl;
+      File2<<(calculate_sus(ising)-mag*mag)/kbT<<std::endl;
     }
   }
   File.close();
+  File2.close();
   //print_file(argv[1],ising);
   return 0;
 }
@@ -42,6 +48,15 @@ double mag=0;
 	}
 }
 return mag/(Nx*Ny);
+}
+double calculate_sus(const matrix & m){
+  double sus=0;
+  for(int x=0;x<Nx;x++){
+    for(int y=0;y<Ny;y++){
+      sus+=(m[x][y]*m[x][y]);
+    }
+  }
+return sus/(Nx*Ny);
 }
 
 
@@ -79,7 +94,7 @@ void print_file(const char * NameArch, matrix & M)
   File.close();
 }
 
-void Time_Step(matrix & m,const double kbT){
+void Time_Step(matrix & m,const double kbT,const double H){
 
   double x=rand()%Nx;  double y=rand()%Ny;
   int xp,xl,yp,yl;
@@ -91,8 +106,8 @@ void Time_Step(matrix & m,const double kbT){
   if(y==Ny-1){yp=0;}
   E=J*(m[xl][y]+m[xp][y]+m[x][yl]+m[x][yp]);
   
-  Eold=m[x][y]*E+h*m[x][y];
-  Enew=-m[x][y]*E-h*m[x][y];
+  Eold=m[x][y]*E+H*m[x][y];
+  Enew=-m[x][y]*E-H*m[x][y];
   dE=Eold-Enew;
   if(dE<0){
     m[x][y]=-m[x][y];
